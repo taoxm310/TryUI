e9a5ae11ec44bf0c49ff1e8a18fdf44fcea7574d
Mandy Tao <taoxiumin2016@gmail.com>
2018-10-24 16:36:42 +0800

    添加icon.vue

---
+++ src/icon.vue
@@ -0,0 +1,22 @@
+<template>
+  <svg class="m-icon">
+    <use :xlink:href="`#i-${name}`"></use>
+  </svg>
+</template>
+
+<script>
+export default {
+  name: 'm-icon',
+  props: ['name']
+}
+</script>
+
+<style lang="scss">
+.m-icon {
+  width: 1em;
+  height: 1em;
+  vertical-align: middle;
+}
+</style>
+
+
