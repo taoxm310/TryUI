a3472079bb76cf4303261e2264b71b8011632014
Mandy Tao <taoxiumin2016@gmail.com>
2018-10-24 16:00:06 +0800

    调整结构使用parcel

---
+++ src/app.js
@@ -0,0 +1,5 @@
+import Vue from 'vue'
+import Button from './button'
+new Vue({
+  el:'#app'
+})
--- src/button.js
+++ src/button.js
@@ -1,3 +1,5 @@
+import Vue from 'vue'
+
 Vue.component('m-button', {
   template:`<button class="m-button"><slot></slot></button>`
 })
