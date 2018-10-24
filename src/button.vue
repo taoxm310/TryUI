08cf11ab4d400b20b35ffc75c80c79d4ab46156d
Mandy Tao <taoxiumin2016@gmail.com>
2018-10-24 16:14:24 +0800

    添加scss

--- src/button.vue
+++ src/button.vue
@@ -1,24 +1,29 @@
 <template>
-  <button class="m-button"><slot></slot></button>
+  <button class="m-button">
+    <slot></slot>
+  </button>
 </template>
 <script>
 export default {
-  name:'m-button'
+  name: 'm-button'
 }
 </script>
-<style>
-    .m-button {
-      height: var(--button-height);
-      padding: 0 1em;
-      font:inherit;
-      border-radius: var(--border-radius);
-      border:1px solid var(--border-color);
-      background-color: var(--button-bg);
-    }
-    .m-button:hover {
-      border-color:var(--border-color-hover)
-    }
-    .m-button:focus {
-      outline:none;
-    }
-  </style>
+<style lang="scss">
+.m-button {
+  height: var(--button-height);
+  padding: 0 1em;
+  font: inherit;
+  border-radius: var(--border-radius);
+  border: 1px solid var(--border-color);
+  background-color: var(--button-bg);
+  &:hover {
+    border-color: var(--border-color-hover);
+  }
+  &:active {
+    background-color: var(--button-active-bg);
+  }
+  &:focus {
+    outline: none;
+  }
+}
+</style>
