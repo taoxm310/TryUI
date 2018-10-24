ef37e0859032c21b1cb7f177a8b2cf775bc465b2
Mandy Tao <taoxiumin2016@gmail.com>
2018-10-24 16:08:59 +0800

    调整为单文件

--- src/button.js
+++
@@ -1,5 +0,0 @@
-import Vue from 'vue'
-
-Vue.component('m-button', {
-  template:`<button class="m-button"><slot></slot></button>`
-})
---
+++ src/button.vue
@@ -0,0 +1,24 @@
+<template>
+  <button class="m-button"><slot></slot></button>
+</template>
+<script>
+export default {
+  name:'m-button'
+}
+</script>
+<style>
+    .m-button {
+      height: var(--button-height);
+      padding: 0 1em;
+      font:inherit;
+      border-radius: var(--border-radius);
+      border:1px solid var(--border-color);
+      background-color: var(--button-bg);
+    }
+    .m-button:hover {
+      border-color:var(--border-color-hover)
+    }
+    .m-button:focus {
+      outline:none;
+    }
+  </style>
