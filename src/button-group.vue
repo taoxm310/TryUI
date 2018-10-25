35ad678700e9c5078a11d2ab84a71b755a3a391a
Mandy Tao <taoxiumin2016@gmail.com>
2018-10-25 16:11:27 +0800

    修改button group hover

--- src/button-group.vue
+++ src/button-group.vue
@@ -5,26 +5,30 @@
 </template>
 <script>
 export default {
-  name:'m-button-group'
+  name: 'm-button-group'
 }
 </script>
 <style lang="scss">
-  .m-button-group {
-    display:inline-flex;
-    vertical-align: middle;
-    >.m-button {
-      border-radius: 0;
-      margin-left:-1px;
-      &:first-child {
-        margin-left:0;
-        border-top-left-radius:var(--border-radius);
-        border-bottom-left-radius: var(--border-radius);
-      }
-      &:last-child {
-        border-top-right-radius:var(--border-radius);
-        border-bottom-right-radius: var(--border-radius);
-      }
+.m-button-group {
+  display: inline-flex;
+  vertical-align: middle;
+  > .m-button {
+    border-radius: 0;
+    margin-left: -1px;
+    position: relative;
+    &:first-child {
+      margin-left: 0;
+      border-top-left-radius: var(--border-radius);
+      border-bottom-left-radius: var(--border-radius);
+    }
+    &:last-child {
+      border-top-right-radius: var(--border-radius);
+      border-bottom-right-radius: var(--border-radius);
+    }
+    &:hover {
+      z-index: 2;
     }
   }
+}
 </style>
 
