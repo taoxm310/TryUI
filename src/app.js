8f74a6b763c09aef712f9836aa2cd54ce382e737
Mandy Tao <taoxiumin2016@gmail.com>
2018-10-26 15:51:46 +0800

    增加自动化测试

--- src/app.js
+++ src/app.js
@@ -17,75 +17,83 @@ const vm = new Vue({
   }
 })
 
-{
-  const Constructor = Vue.extend(Button)
-  const vm = new Constructor({
-    propsData: {
-      icon: 'download'
-    }
-  }).$mount()
+try {
+  {
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+      propsData: {
+        icon: 'download'
+      }
+    }).$mount()
 
-  const useElement = vm.$el.querySelector('use')
-  expect(useElement.getAttribute('xlink:href')).to.equal('#i-download')
-  vm.$destroy()
-}
+    const useElement = vm.$el.querySelector('use')
+    expect(useElement.getAttribute('xlink:href')).to.equal('#i-download')
+    vm.$destroy()
+  }
 
-{
-  const Constructor = Vue.extend(Button)
-  const vm = new Constructor({
-    propsData: {
-      icon: 'download',
-      loading: true
-    }
-  }).$mount()
+  {
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+      propsData: {
+        icon: 'download',
+        loading: true
+      }
+    }).$mount()
 
-  const useElement = vm.$el.querySelector('use')
-  expect(useElement.getAttribute('xlink:href')).to.equal('#i-loading')
-  vm.$destroy()
-}
+    const useElement = vm.$el.querySelector('use')
+    expect(useElement.getAttribute('xlink:href')).to.equal('#i-loading')
+    vm.$destroy()
+  }
 
-{
-  const div = document.createElement('div')
-  document.body.appendChild(div)
-  const Constructor = Vue.extend(Button)
-  const vm = new Constructor({
-    propsData: {
-      icon: 'loading'
-    }
-  }).$mount(div)
-  const icon = vm.$el.querySelector('svg')
-  expect(window.getComputedStyle(icon).order).to.equal('1')
-  vm.$el.remove()
-  vm.$destroy()
-}
+  {
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+      propsData: {
+        icon: 'loading'
+      }
+    }).$mount(div)
+    const icon = vm.$el.querySelector('svg')
+    expect(window.getComputedStyle(icon).order).to.equal('1')
+    vm.$el.remove()
+    vm.$destroy()
+  }
 
-{
-  const div = document.createElement('div')
-  document.body.appendChild(div)
-  const Constructor = Vue.extend(Button)
-  const vm = new Constructor({
-    propsData: {
-      icon: 'loading',
-      iconPosition: 'right'
-    }
-  }).$mount(div)
-  const icon = vm.$el.querySelector('svg')
-  expect(window.getComputedStyle(icon).order).to.equal('2')
-  vm.$el.remove()
-  vm.$destroy()
-}
+  {
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+      propsData: {
+        icon: 'loading',
+        iconPosition: 'right'
+      }
+    }).$mount(div)
+    const icon = vm.$el.querySelector('svg')
+    expect(window.getComputedStyle(icon).order).to.equal('2')
+    vm.$el.remove()
+    vm.$destroy()
+  }
 
-{
-  const Constructor = Vue.extend(Button)
-  const vm = new Constructor({
-    propsData: {
-      icon: 'download',
-      loading: true
-    }
-  }).$mount()
-  let spy = chai.spy(function() {})
-  vm.$on('click', spy)
-  let button = vm.$el
-  button.click()
-  expect(spy).to.have.been.called()
+  {
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+      propsData: {
+        icon: 'download',
+        loading: true
+      }
+    }).$mount()
+    let spy = chai.spy(function() {})
+    vm.$on('click', spy)
+    let button = vm.$el
+    button.click()
+    expect(spy).to.have.been.called()
+  }
+} catch (error) {
+  window.errors = [error]
+} finally {
+  window.errors.forEach(error => {
+    console.error(error.message)
+  })
 }
