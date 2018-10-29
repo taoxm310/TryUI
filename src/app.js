693949dc7ba30c1bfd8e4a30aebf7fc6969d9319
Mandy Tao <taoxiumin2016@gmail.com>
2018-10-29 18:20:49 +0800

    增加input基本结构

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Button from './button.vue'
 import ButtonGroup from './button-group.vue'
+import Input from './input.vue'
 
 import chai from 'chai'
 import spies from 'chai-spies'
@@ -9,6 +10,7 @@ const expect = chai.expect
 
 Vue.component('m-button', Button)
 Vue.component('m-button-group', ButtonGroup)
+Vue.component('m-input', Input)
 const vm = new Vue({
   el: '#app',
   data: {
@@ -16,85 +18,3 @@ const vm = new Vue({
     loading2: true
   }
 })
-
-try {
-  {
-    const Constructor = Vue.extend(Button)
-    const vm = new Constructor({
-      propsData: {
-        icon: 'download'
-      }
-    }).$mount()
-
-    const useElement = vm.$el.querySelector('use')
-    expect(useElement.getAttribute('xlink:href')).to.equal('#i-download')
-    vm.$destroy()
-  }
-
-  {
-    const Constructor = Vue.extend(Button)
-    const vm = new Constructor({
-      propsData: {
-        icon: 'download',
-        loading: true
-      }
-    }).$mount()
-
-    const useElement = vm.$el.querySelector('use')
-    expect(useElement.getAttribute('xlink:href')).to.equal('#i-loading')
-    vm.$destroy()
-  }
-
-  {
-    const div = document.createElement('div')
-    document.body.appendChild(div)
-    const Constructor = Vue.extend(Button)
-    const vm = new Constructor({
-      propsData: {
-        icon: 'loading'
-      }
-    }).$mount(div)
-    const icon = vm.$el.querySelector('svg')
-    expect(window.getComputedStyle(icon).order).to.equal('1')
-    vm.$el.remove()
-    vm.$destroy()
-  }
-
-  {
-    const div = document.createElement('div')
-    document.body.appendChild(div)
-    const Constructor = Vue.extend(Button)
-    const vm = new Constructor({
-      propsData: {
-        icon: 'loading',
-        iconPosition: 'right'
-      }
-    }).$mount(div)
-    const icon = vm.$el.querySelector('svg')
-    expect(window.getComputedStyle(icon).order).to.equal('2')
-    vm.$el.remove()
-    vm.$destroy()
-  }
-
-  {
-    const Constructor = Vue.extend(Button)
-    const vm = new Constructor({
-      propsData: {
-        icon: 'download',
-        loading: true
-      }
-    }).$mount()
-    let spy = chai.spy(function() {})
-    vm.$on('click', spy)
-    let button = vm.$el
-    button.click()
-    expect(spy).to.have.been.called()
-  }
-} catch (error) {
-  window.errors = [error]
-} finally {
-  window.errors &&
-    window.errors.forEach(error => {
-      console.error(error.message)
-    })
-}
---
+++ src/input.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="m-input__wrapper" :class="{error}">
+    <input type="text" :value="value" class="m-input" :disabled="disabled" :readonly="readonly">
+    <template v-if="error">
+      <Icon name="error" class="icon-error"></Icon>
+      <span class="error-message">{{error}}</span>
+    </template>
+  </div>
+</template>
+<script>
+import Icon from './icon'
+export default {
+  name: 'm-input',
+  components: {
+    Icon
+  },
+  props: {
+    value: {
+      type: String
+    },
+    error: {
+      type: String
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    readonly: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+</style>
+
+
