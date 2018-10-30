01602929bf92d2ed1493a166707dc33be6b4842f
Mandy Tao <taoxiumin2016@gmail.com>
2018-10-30 18:25:12 +0800

    增加 offset 和 gutter 功能

--- src/col.vue
+++ src/col.vue
@@ -1,6 +1,8 @@
 <template>
-  <div class="m-col" :class="`m-col-${span}`">
-    <slot></slot>
+  <div class="m-col" :class="classList" :style="style">
+    <div style="height:100px; border:1px solid red;">
+      <slot></slot>
+    </div>
   </div>
 </template>
 
@@ -10,6 +12,45 @@ export default {
   props: {
     span: {
       type: [String, Number]
+    },
+    offset: {
+      type: [String, Number]
+    }
+  },
+  data() {
+    return {
+      gutter: 0
+    }
+  },
+  created() {
+    console.log('col created')
+  },
+  mounted() {
+    console.log('col mounted')
+  },
+  computed: {
+    classList() {
+      let list = []
+      ;[('span', 'offset')].forEach(prop => {
+        if (this[prop] || this[prop] === 0) {
+          list.push(
+            prop !== 'span'
+              ? `m-col-${prop}-${this[prop]}`
+              : `m-col-${this[prop]}`
+          )
+        }
+      })
+      return list
+    },
+    style() {
+      let result = {}
+      if (this.gutter) {
+        result.paddingLeft = `${this.gutter / 2}px`
+        result.paddingRight = result.paddingLeft
+      }
+      console.log(result)
+
+      return result
     }
   }
 }
@@ -17,14 +58,17 @@ export default {
 
 <style lang="scss" scoped>
 .m-col {
-  height: 50px;
-  background-color: grey;
   width: 50%;
-  border: 1px solid red;
   $class-prefix: 'm-col-';
   @for $n from 1 through 24 {
     &.#{$class-prefix}#{$n} {
       width: ($n/24) * 100%;
+    }
+  }
+  $class-prefix: 'm-col-offset-';
+  @for $n from 1 through 24 {
+    &.#{$class-prefix}#{$n} {
+      margin-left: ($n/24) * 100%;
     }
   }
 }
--- src/row.vue
+++ src/row.vue
@@ -1,12 +1,38 @@
 <template>
-  <div class="m-row">
+  <div class="m-row" :style="style">
     <slot></slot>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'm-row'
+  name: 'm-row',
+  props: {
+    gutter: {
+      type: [String, Number]
+    }
+  },
+  created() {
+    console.log('row created')
+  },
+  mounted() {
+    console.log('row mounted')
+    console.log('children', this.$children)
+    this.$children.forEach(vm => {
+      vm.gutter = this.gutter
+    })
+  },
+  computed: {
+    style() {
+      const result = {}
+      if (this.gutter) {
+        result.marginLeft = `-${this.gutter / 2}px`
+        result.marginRight = result.marginLeft
+      }
+      console.log(result)
+      return result
+    }
+  }
 }
 </script>
 
