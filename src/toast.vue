d622411387c39077811ea5af6a0b6fb9f16bf0f9
Mandy Tao <taoxiumin2016@gmail.com>
2018-11-07 18:15:03 +0800

    给 toast 添加基本样式和开关功能

--- src/toast.vue
+++ src/toast.vue
@@ -1,20 +1,79 @@
 <template>
-  <div class="m-toast">
-    <slot></slot>
+  <div class="m-toast" ref="wrapper">
+    <div class="message">
+      <slot v-if="!enableHTML"></slot>
+      <div v-else v-html="$slots.default[0]"></div>
+    </div>
+    <div class="line" ref="line"></div>
+    <span class="close" v-if="closeButton" @click="onClickClose">{{closeButton.text}}</span>
   </div>
 </template>
 <script>
 export default {
-  name: 'MToast'
+  name: 'MToast',
+  props: {
+    autoClose: {
+      type: Boolean,
+      default: true
+    },
+    autoCloseDelay: {
+      type: Number,
+      default: 50
+    },
+    closeButton: {
+      type: Object,
+      default() {
+        return {
+          text: '关闭',
+          callback: undefined
+        }
+      }
+    },
+    enableHTML: {
+      type: Boolean,
+      default: false
+    }
+  },
+  created() {},
+  mounted() {
+    this.updateStyle()
+    this.startTimer()
+  },
+  methods: {
+    updateStyle() {
+      this.$nextTick(() => {
+        this.$refs.line.style.height = `${
+          this.$refs.wrapper.getBoundingClientRect().height
+        }px`
+      })
+    },
+    startTimer() {
+      if (this.autoClose) {
+        setTimeout(() => {
+          this.close
+        }, this.autoCloseDelay * 1000)
+      }
+    },
+    close() {
+      this.$el.remove()
+      this.$destroy
+    },
+    onClickClose() {
+      this.close()
+      if (this.closeButton && typeof this.closeButton.callback === 'function') {
+        this.closeButton.callback(this)
+      }
+    }
+  }
 }
 </script>
 <style lang="scss" scoped>
 $font-size: 14px;
-$toast-height: 40px;
+$toast-min-height: 40px;
 $toast-bg: rgba(0, 0, 0, 0.75);
 .m-toast {
   font-size: $font-size;
-  height: $toast-height;
+  min-height: $toast-min-height;
   line-height: 1.8;
   position: fixed;
   top: 0;
@@ -27,6 +86,18 @@ $toast-bg: rgba(0, 0, 0, 0.75);
   border-radius: 4px;
   box-shadow: 0 0 3px 0 rgba(0, 0, 0, 0.5);
   padding: 0 16px;
+  .message {
+    padding: 8px 0;
+  }
+  .close {
+    padding-left: 16px;
+    flex-shrink: 0;
+  }
+  .line {
+    height: 100%;
+    border-left: 1px solid #666;
+    margin-left: 16px;
+  }
 }
 </style>
 
