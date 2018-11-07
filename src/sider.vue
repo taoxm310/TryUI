76f37b200da77c57bf79737f28c9075a34f042ee
Mandy Tao <taoxiumin2016@gmail.com>
2018-11-07 16:15:30 +0800

    添加 sider 简单动画

--- src/sider.vue
+++ src/sider.vue
@@ -1,16 +1,39 @@
 <template>
-  <div class="m-sider">
-    <slot></slot>
-  </div>
+  <transition name="slide">
+    <div class="m-sider" v-if="visible">
+      <slot></slot>
+      <button @click="visible=!visible">close</button>
+    </div>
+  </transition>
 </template>
 <script>
 export default {
-  name: 'MSider'
+  name: 'MSider',
+  data() {
+    return {
+      visible: true
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
 .m-sider {
+  position: relative;
+  > button {
+    position: absolute;
+    top: 0;
+    right: 0;
+  }
+}
+.slide-enter-active,
+.slide-leave-active {
+  transition: all 0.5s;
+}
+
+.slide-enter,
+.slide-leave-to {
+  margin-left: -150px;
 }
 </style>
 
