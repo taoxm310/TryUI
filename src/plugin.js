74f7984cdf469f03f94511664954c4a11623e8b2
Mandy Tao <taoxiumin2016@gmail.com>
2018-11-08 16:14:16 +0800

    调整为只能出现一个 toast

--- src/plugin.js
+++ src/plugin.js
@@ -1,15 +1,28 @@
 import Toast from './toast.vue'
-
+let currentToast
 export default {
   install(Vue, options) {
     Vue.prototype.$toast = function(message, toastOptions) {
-      let Constructor = Vue.extend(Toast)
-      let toast = new Constructor({
-        propsData: toastOptions
+      if (currentToast) {
+        currentToast.close()
+      }
+      currentToast = createToast({
+        Vue,
+        message,
+        toastOptions
       })
-      toast.$slots.default = [message]
-      toast.$mount()
-      document.body.appendChild(toast.$el)
     }
   }
+}
+
+// helpers
+function createToast({ Vue, message, toastOptions }) {
+  let Constructor = Vue.extend(Toast)
+  let toast = new Constructor({
+    propsData: toastOptions
+  })
+  toast.$slots.default = [message]
+  toast.$mount()
+  document.body.appendChild(toast.$el)
+  return toast
 }
