281c03fdcfe9c443140c621dcd030645520ab00f
Mandy Tao <taoxiumin2016@gmail.com>
2018-11-26 10:54:15 +0800

    添加 tooltip 基本样式

--- src/tooltip.vue
+++ src/tooltip.vue
@@ -3,44 +3,60 @@
     <div class="content-wrapper" v-if="visible" ref="contentWrapper">
       <slot name="content"></slot>
     </div>
-    <span ref="triggerWrapper">
+    <span ref="triggerWrapper" style="display:inline-block">
       <slot></slot>
     </span>
   </div>
 </template>
 <script>
 export default {
-  name:'MTooltip',
-  data(){
+  name: 'MTooltip',
+  data() {
     return {
-      visible:false
+      visible: false
     }
   },
-  methods:{
-    placeContent(){
+  methods: {
+    placeContent() {
       document.body.appendChild(this.$refs.contentWrapper)
-      let {width, top,left,height} = this.$refs.triggerWrapper.getBoundingClientRect()
+      let {
+        width,
+        top,
+        left,
+        height
+      } = this.$refs.triggerWrapper.getBoundingClientRect()
       this.$refs.contentWrapper.style.left = left + window.scrollX + 'px'
       this.$refs.contentWrapper.style.top = top + window.scrollY + 'px'
     },
-    open(){
-      this.visible = true,
-      this.$nextTick(() => {
-        this.placeContent()
-        document.addEventListener('click',this.onClickDocument)
-      })
+    open() {
+      ;(this.visible = true),
+        this.$nextTick(() => {
+          this.placeContent()
+          document.addEventListener('click', this.onClickDocument)
+        })
     },
     close() {
-      this.visible = false,
-      document.removeEventListener('click', this.onClickDocument)
+      ;(this.visible = false),
+        document.removeEventListener('click', this.onClickDocument)
     },
-    onClickDocument(e){
-      if (this.$refs.tooltip && this.$refs.tooltip === e.target || this.$refs.tooltip.contains(e.target) ) {
+    onClickDocument(e) {
+      if (
+        this.$refs.popover &&
+        (this.$refs.popover === e.target ||
+          this.$refs.popover.contains(e.target))
+      ) {
+        return
+      }
+      if (
+        this.$refs.contentWrapper &&
+        (this.$refs.contentWrapper === e.target ||
+          this.$refs.contentWrapper.contains(e.target))
+      ) {
         return
       }
       this.close()
     },
-    onClick(event){
+    onClick(event) {
       if (this.$refs.triggerWrapper.contains(event.target)) {
         if (this.visible === true) {
           this.close()
@@ -53,16 +69,43 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
-  .m-tooltip {
-    display: inline-block;
-    vertical-align: top;
-    position: relative;
+$border-color: #333;
+$border-radius: 4px;
+.m-tooltip {
+  display: inline-block;
+  vertical-align: top;
+  position: relative;
+}
+.content-wrapper {
+  position: absolute;
+  border-radius: 3px;
+  border: 1px solid $border-color;
+  border-radius: $border-radius;
+  filter: drop-shadow(0 1px 1px rgba(0, 0, 0, 0.5));
+  background-color: white;
+  transform: translateY(-100%);
+  margin-top: -10px;
+  padding: 0.5em 1em;
+  max-width: 20em;
+  word-break: break-all;
+  &:after,
+  &::before {
+    content: '';
+    display: block;
+    border: 10px solid transparent;
+    width: 0;
+    height: 0;
+    position: absolute;
+    left: 10px;
   }
-  .content-wrapper {
-      position: absolute;
-      border: 1px solid grey;
-      border-radius: 3px;
-      box-shadow: 0 0 4px rgba(0, 0, 0, .5)
-    }
+  &:before {
+    border-top-color: black;
+    top: 100%;
+  }
+  &:after {
+    border-top-color: white;
+    top: calc(100% - 1px);
+  }
+}
 </style>
 
